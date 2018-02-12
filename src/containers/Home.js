c99502393fcf4048221b983636f028ed92a676e4
Janie Tito <jtito@hotmail.com>
2018-02-13 11:34:17 +1300

    feat($feat): Added a grid in the bottom section containing icons and text

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -6,6 +6,7 @@ import chevronNext from "../img/chevron-next.svg";
 import typDash from "../img/typ-dash.png";
 import homeDog from "../img/dog1.png";
 import team from "../img/team.png";
+import lightning from "../img/lightning.png";
 
 const Container = styled.div(props => ({
   display: "flex",
@@ -115,7 +116,71 @@ const SubText = styled.div`
 `;
 
 const Bottom = styled.div`
-  background: blue;
+  display: -webkit-box;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -o-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-flex-direction: column;
+  -moz-flex-direction: column;
+  -ms-flex-direction: column;
+  -o-flex-direction: column;
+  flex-direction: column;
+`;
+
+const GridRow = styled.div`
+  background-color: white;
+  -webkit-flex: 5;
+  -moz-flex: 5;
+  -ms-flex: 5;
+  -o-flex: 5;
+  flex: 5;
+  display: -webkit-box;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-flex-direction: row;
+  -moz-flex-direction: row;
+  -ms-flex-direction: row;
+  -o-flex-direction: row;
+  flex-direction: row;
+  width: 80%;
+  margin: auto;
+`;
+
+const Square = styled.div`
+  background-color: white;
+  -webkit-flex: 4;
+  -moz-flex: 4;
+  -ms-flex: 4;
+  -o-flex: 4;
+  flex: 4;
+  display: -webkit-box;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  text-align: center;
+  align-items: center;
+  -webkit-flex-flow: column wrap;
+  -moz-flex-flow: column wrap;
+  -ms-flex-flow: column wrap;
+  -o-flex-flow: column wrap;
+  flex-flow: column wrap;
+  margin: 1em;
+`;
+
+const SquareHeading = styled.div`
+  text-transform: uppercase;
+  font-size: 1em;
+  margin-top: 5px;
+`;
+
+const SquareSubheading = styled.div`
+  font-size: 0.8em;
+  margin-top: 5px;
 `;
 
 export default withSiteData(() => (
@@ -157,7 +222,88 @@ export default withSiteData(() => (
       </SubText>
     </Middle>
     <Bottom>
-      <h1>Bottom Section</h1>
+      <GridRow>
+        <Square>
+          <img src={lightning} alt="lightning" style={{ width: "30px" }} />
+          <SquareHeading>Easy to use</SquareHeading>
+          <SquareSubheading>
+            Our payroll platform is easy and user-friendly, and makes managing
+            your payroll stress free and lightning fast.
+          </SquareSubheading>
+        </Square>
+        <Square>
+          <img src={lightning} alt="lightning" style={{ width: "30px" }} />
+          <SquareHeading>Friendly help desk</SquareHeading>
+          <SquareSubheading>
+            Our amazing support team always go the extra mile to help you - and
+            it doesn't cost you any extra to have them on hand.
+          </SquareSubheading>
+        </Square>
+        <Square>
+          <img src={lightning} alt="lightning" style={{ width: "30px" }} />
+          <SquareHeading>Calculate Leave</SquareHeading>
+          <SquareSubheading>
+            We calculate and monitor employee leave according to the Holidays
+            Act, helping you stay on top of leave requirements.
+          </SquareSubheading>
+        </Square>
+      </GridRow>
+      <GridRow class="row">
+        <Square>
+          <img src={lightning} alt="lightning" style={{ width: "30px" }} />
+          <SquareHeading>File Employer Returns</SquareHeading>
+          <SquareSubheading>
+            We calculate all PAYE, Kiwisaver, Student loans and Child Support,
+            and file it with the IRD. We keep you compliant. We even do MoJ
+            fines.
+          </SquareSubheading>
+        </Square>
+        <Square>
+          <img src={lightning} alt="lightning" style={{ width: "30px" }} />
+          <SquareHeading>Employee Portal</SquareHeading>
+          <SquareSubheading>
+            Your employees can upload their own hours, get payslips, and request
+            annual leave - all you need to do is check and authorise it.
+          </SquareSubheading>
+        </Square>
+        <Square>
+          <img src={lightning} alt="lightning" style={{ width: "30px" }} />
+          <SquareHeading>Transfer Wages</SquareHeading>
+          <SquareSubheading>
+            We pay all your employees directly, and email them payslips - you
+            can even put your business logo on them.
+          </SquareSubheading>
+        </Square>
+      </GridRow>
+      <GridRow class="row">
+        <Square>
+          <img src={lightning} alt="lightning" style={{ width: "30px" }} />
+          <SquareHeading>Giving Made Simple</SquareHeading>
+          <SquareSubheading>
+            It's effortless for your employees to donate regularly to their
+            favourite charity. They get an immediate tax rebate on each
+            donation.
+          </SquareSubheading>
+        </Square>
+        <Square>
+          <img src={lightning} alt="lightning" style={{ width: "30px" }} />
+          <SquareHeading>24/7 Access</SquareHeading>
+          <SquareSubheading>
+            Our platform is cloud-based, which means you access your payroll
+            from anywhere, anytime. All you need is a device and an internet
+            connection.
+          </SquareSubheading>
+        </Square>
+        <Square>
+          <img src={lightning} alt="lightning" style={{ width: "30px" }} />
+          <SquareHeading>Good for any business</SquareHeading>
+          <SquareSubheading>
+            Payroll solutions for trades and skilled labour, business and
+            investment, hospitality, education, retail, social services and many
+            more.
+          </SquareSubheading>
+        </Square>
+      </GridRow>
     </Bottom>
   </Container>
 ));
