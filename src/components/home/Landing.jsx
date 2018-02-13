c0450176ba2cb4f6e022be7a3f1b84107ff9891d
Janie Tito <jtito@hotmail.com>
2018-02-14 11:42:50 +1300

    feat($layout): Imported resources into Landing component

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import { withSiteData, Link } from 'react-static';
+import styled from 'react-emotion';
+import { Images } from '../../assets';
+import content from '../../content';
+import colors from '../../styles/colors';
 import { Section, Container } from '../commons';
 
 const Landing = () => (
