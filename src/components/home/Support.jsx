2daf9beb7ab7b6e2760f619b182913258bfe6e97
Janie Tito <jtito@hotmail.com>
2018-02-15 17:43:46 +1300

    feat($style): Finished styling for support team (middle) section

--- src/components/home/Support.jsx
+++ src/components/home/Support.jsx
@@ -7,12 +7,14 @@ import background2 from '../../assets/images/background2.png';
 
 
 const Background = styled.div`
+  display: flex;
   flex: 1;
   background-image: url(${background2});
   background-repeat: no-repeat;
   z-index: 1;
   width: 100%;
   height: 700px;
+  align-items: center;
 `;
 
 const Middle = styled.div`
@@ -20,7 +22,17 @@ const Middle = styled.div`
   width: 100%;
   height: 200px;
   background: rgba(69, 62, 64, 0.4);
-  padding-top: 2em;
+  position: relative;
+  &::before {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 0;
+    border-top: 1px solid white;
+    width: 100%;
+    transform: translateY(-50%);
+  }
+  display: flex;
 `;
 
 
@@ -29,7 +41,6 @@ const MainText = styled.div`
   font-size: 3em;
   font-weight: bold;
   max-height: 150px;
-  padding-bottom: 0.5em;
 `;
 
 const SubText = styled.div`
@@ -37,20 +48,27 @@ const SubText = styled.div`
   color: white;
   font-size: 1.6em;
   padding-top: 1em;
+  opacity: 0.7;
 `;
+
+const NewContent = styled.div`
+  color: white;
+  font-size: 1.6em;
+`
+
 
 const Support = () => (
   <Section>
     <Background>
       <Middle>
         <Container>
-
           <MainText>We have a really great support team.</MainText>
           <SubText>
-            We are proud of these humans in the Wellington office. Our customers love them. Link to
-            new content >
+            We are proud of these humans in the Wellington office. Our customers love them.
           </SubText>
-
+          <NewContent>
+            Link to new content
+          </NewContent>
         </Container>
       </Middle>
     </Background>
