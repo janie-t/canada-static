d80be8ecd5345a1a924d5b2a5c2c963d05fafd23
Janie Tito <jtito@hotmail.com>
2018-02-15 14:51:56 +1300

    feat($style): Added style and content to middle section - support team

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,15 +1,15 @@
 import logo from './images/logo.png';
 import typDash from './images/typ-dash.png';
 import homeDog from './images/dog1.png';
-import team from './images/team.png';
 import lightning from './images/lightning.png';
 
 const Images = {
   logo,
   typDash,
   homeDog,
-  team,
 };
-const Icons = { lightning };
+const Icons = {
+  lightning,
+};
 
 export { Images, Icons };
--- src/components/home/Support.jsx
+++ src/components/home/Support.jsx
@@ -1,9 +1,71 @@
 import React from 'react';
+import styled from 'react-emotion';
+
 import { Section, Container } from '../commons';
+
+import background2 from '../../assets/images/background2.png';
+
+
+const Background = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  background-image: url(${background2});
+  background-repeat: no-repeat;
+  background-size: 100%;
+  z-index: 1;
+  width: 100%;
+  height: 700px;
+`;
+
+const Middle = styled.div`
+  width: 100%;
+  height: 200px;
+  background: #453e40;
+  padding-top: 1em;
+  position: relative;
+  &:before {
+    z-index: -1;
+    content: '';
+    position: absolute;
+    opacity: 0.4;
+  };
+`;
+
+
+const MainText = styled.div`
+  color: white;
+  font-size: 3em;
+  font-weight: bold;
+  border-bottom: solid white 1px;
+  max-height: 150px;
+  padding-bottom: 0.5em;
+`;
+
+const SubText = styled.div`
+  width: 100%;
+  background: #453e40;
+  color: white;
+  font-size: 1.6em;
+  padding-top: 1em;
+`;
 
 const Support = () => (
   <Section>
-    <Container>Support Team!</Container>
+    <Background>
+      <Middle>
+        <Container>
+
+          <MainText>We have a really great support team.</MainText>
+          <SubText>
+            We are proud of these humans in the Wellington office. Our customers love them. Link to
+            new content >
+          </SubText>
+
+        </Container>
+      </Middle>
+    </Background>
   </Section>
 );
 
