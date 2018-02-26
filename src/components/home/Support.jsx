4773ff5556905441ea1c5b933c58413e07f83838
Janie Tito <jtito@hotmail.com>
2018-02-26 20:57:36 +1300

    feat($style): Added mobile view style to features section and footer

--- src/components/home/Support.jsx
+++ src/components/home/Support.jsx
@@ -10,44 +10,60 @@ const Section = styled.div`
   background:  url(${Images.support});
   background-repeat: no-repeat;
   background-size: cover;
-  padding-top: 10%;
-
+  padding-top: 30%;
+  @media (min-width: 576px) {
+    padding-top: 10%;
+  };
 `;
 
 const Container = styled.div`
   align-items: center;
   z-index: 1;
-  padding: 2em 1em;
+  padding-top: 1em;
+  @media (min-width: 576px) {
+    padding: 2em 1em;
+  }
 `;
 
 const Middle = styled.div`
   text-align: center;
   width: 100%;
-  height: 200px;
   background: rgba(69, 62, 64, 0.4);
-
   color: ${colors.WHITE};
+  @media (min-width: 576px) {
+    height: 200px;
+  };
 `;
 
 
 const MainText = styled.div`
+font-weight: bold;
+border-bottom: 1px solid;
+border-color: ${colors.WHITE};
+border-width: 100%;
+font-size: 1.8em;
+@media (min-width: 576px) {
   font-size: 3em;
-  font-weight: bold;
   max-height: 150px;
-  border-bottom: 1px solid;
-  border-color: ${colors.WHITE};
-  border-width: 100%;
+};
 `;
 
 const SubText = styled.div`
   width: 100 %;
-  font-size: 1.6em;
-  padding-top: 1em;
+  font-size: 1.3em;
+  padding-top: 0.5em;
   opacity: 0.7;
+  @media (min-width: 576px) {
+    font-size: 1.6em;
+    padding-top: 1em;
+  };
 `;
 
 const NewContent = styled.div`
-  font-size: 1.6em;
+  font-size: 1.1em;
+  @media (min-width: 576px) {
+    font-size: 1.6em;
+  };
 `
 
 
