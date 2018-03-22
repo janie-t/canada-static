542b9656bb04ad7557d77144e4346a8cabff0af7
saima-t <saimatahir08@gmail.com>
2018-03-22 20:55:12 +1300

    new changes

--- src/components/home/Credentials.jsx
+++ src/components/home/Credentials.jsx
@@ -9,10 +9,14 @@ import colors from '../../styles/colors';
 
 
 const Heading = styled.div`
+
+  padding: 10;
+  margin: 10rem;
   font-size: 0.5em;
   text-align: right;
   color: ${colors.WHITE};
   font-weight: bolder;
+  margin:1em;
   @media (min-width: 576px) {
     color: black;
     font-size: 1em;
@@ -21,6 +25,7 @@ const Heading = styled.div`
 
   }
 `;
+
 
 export default withSiteData(() => {
   const CRED = content('credentials.address');
@@ -32,7 +37,8 @@ export default withSiteData(() => {
           <Link to="/how" className="menu-item">News</Link>
           <Link to="/about" className="menu-item">About Us</Link>
           <Link to="./contact" className="menu-item">Contact</Link>
-          </Heading>
+        
+        </Heading>
           </div>
       );     
 });
